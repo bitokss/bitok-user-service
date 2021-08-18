93298bdf24761f6aa5f6efb9c1d2b92feb793e63
alidevjimmy <hereisjimmymartin@gmail.com>
2021-08-18 20:12:32 +0430

    adding user controller

---
+++ src/controllers/v1/users_controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import "github.com/labstack/echo/v4"
+
+var UserController userControllerInterface = &userController{}
+
+type userControllerInterface interface {
+	Find(c echo.Context) error
+}
+
+type userController struct {}
+
+func (u *userController) Find(c echo.Context) error {
+	return c.JSON(200 , "user :)")
+}
+
